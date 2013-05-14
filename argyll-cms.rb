3dd6ea75255a9f6a7274df186f5cc2139e49614a
Misty De Meo <mistydemeo@gmail.com>
2013-05-14 11:34:23 -0500

    argyll-cms 1.5.1

--- argyll-cms.rb
+++ argyll-cms.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class ArgyllCms < Formula
   homepage 'http://www.argyllcms.com/'
-  url 'http://www.argyllcms.com/Argyll_V1.4.0_osx10.4_i86_bin.tgz'
-  sha1 '6cd8e44678b8983bb31ae3bbe9fdce42f35d5108'
-  version '1.4.0'
+  url 'http://www.argyllcms.com/Argyll_V1.5.1_osx10.6_x86_64_bin.tgz'
+  sha1 '505c3ec48119e2cb318f4a9a60cba220302213ba'
+  version '1.5.1'
 
   def install
     rm "bin/License.txt" # no, we don't want this in bin/
