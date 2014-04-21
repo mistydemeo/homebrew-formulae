0545536323f040a296a2fd813291f3a995729ff6
Misty De Meo <mistydemeo@gmail.com>
2014-04-20 17:22:55 -0700

    org2xm 1.0

---
+++ org2xm.rb
@@ -0,0 +1,30 @@
+require "formula"
+
+class Org2xm < Formula
+  homepage "http://rrrola.wz.cz/downloads.html"
+  url "http://rrrola.wz.cz/files/org2xm.zip"
+  version "1.0"
+  sha1 "a0c64aa12a01fc580193764491ee3153fa99e05e"
+
+  fails_with :clang do
+    cause "clang does not permit nested function definitions"
+  end
+
+  def install
+    # gcc-4.2 will complain about nested functions by default,
+    # while clang simply refuses to compile it at all
+    ENV.append_to_cflags "-fnested-functions"
+
+    cd "src" do
+      system "make", "org2xm", "wave100"
+      bin.install "org2xm", "wave100"
+    end
+
+    cd "samples" do
+      system bin/"wave100" rescue nil # exits 255 on success!
+    end
+
+    # Full Cave Story soundtrack, plus requisite samples
+    (share/"org2xm").install "org", "samples"
+  end
+end
