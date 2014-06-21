a9236b33373123285bbaa6d8a36e2065ff8d705d
Misty De Meo <mistydemeo@gmail.com>
2014-06-21 13:35:25 -0700

    org2xm: wrap script so it can be called from anywhere

--- org2xm.rb
+++ org2xm.rb
@@ -6,7 +6,6 @@ class Org2xm < Formula
   version "1.0"
   sha1 "a0c64aa12a01fc580193764491ee3153fa99e05e"
 
-  # -fnested-functions won't coerce clang to build this.
   fails_with :clang do
     cause "clang does not permit nested function definitions"
   end
@@ -18,14 +17,31 @@ class Org2xm < Formula
 
     cd "src" do
       system "make", "org2xm", "wave100"
-      bin.install "org2xm", "wave100"
+      libexec.install "org2xm", "wave100"
+
+      # org2xm has to be run in a specific directory; this wrapper
+      # lets it be called from anywhere
+      (bin/'org2xm').write <<-EOS.undent
+      #!/usr/bin/env ruby
+
+      ARGV[0] = File.expand_path(ARGV[0]) if ARGV[0]
+      # org2xm must be run in this directory in order to find its samples.
+      Dir.chdir "#{share}/org2xm"
+      exec "#{libexec}/org2xm", *ARGV
+      EOS
     end
 
     cd "samples" do
-      system bin/"wave100" rescue nil # exits 255 on success!
+      system libexec/"wave100" rescue nil # exits 255 on success!
     end
 
     # Full Cave Story soundtrack, plus requisite samples
     (share/"org2xm").install "org", "samples"
+  end
+
+  def caveats; <<-EOS.undent
+    The full Cave Story soundtrack has been installed to:
+      #{share}/org2xm/org
+  EOS
   end
 end
