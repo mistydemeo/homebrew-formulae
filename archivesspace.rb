de50b31a7566e5239900fa3f186b45192456a8c3
Misty De Meo <mistydemeo@gmail.com>
2015-01-30 18:40:39 -0800

    archivesspace: remove plist

--- archivesspace.rb
+++ archivesspace.rb
@@ -10,22 +10,4 @@ class Archivesspace < Formula
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/archivesspace.sh" => "archivesspace"
   end
-
-  def plist; <<-EOS.undent
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/archivesspace</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-    </dict>
-    </plist>
-    EOS
-  end
 end
