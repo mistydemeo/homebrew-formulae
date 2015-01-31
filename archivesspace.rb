ba44ccf1887ab1e1908cf10dc440d1bdad73af30
Misty De Meo <mistydemeo@gmail.com>
2015-01-30 18:07:54 -0800

    archivesspace 1.1.2

--- archivesspace.rb
+++ archivesspace.rb
@@ -1,13 +1,31 @@
 class Archivesspace < Formula
   homepage "http://archivesspace.org/"
-  url "https://github.com/archivesspace/archivesspace/releases/download/v1.1.1/archivesspace-v1.1.1.zip"
-  version "1.1.1"
-  sha1 "aa74577b7da97dac18f1d98251145d398a2d170e"
+  url "https://github.com/archivesspace/archivesspace/releases/download/v1.1.2/archivesspace-v1.1.2.zip"
+  version "1.1.2"
+  sha1 "7a000e0af9765842f7da1a06fcf102d9c2199213"
 
   depends_on :java => "1.6"
 
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/archivesspace.sh" => "archivesspace"
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/archivesspace</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
   end
 end
