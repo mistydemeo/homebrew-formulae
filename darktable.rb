722d29cbcc48bde3efaa94306dac21257f6d16ce
Alex Dunn <dunn.alex@gmail.com>
2015-04-20 11:20:37 -0700

    darktable 1.6.6

--- darktable.rb
+++ darktable.rb
@@ -1,31 +1,36 @@
-require 'formula'
-
 class Darktable < Formula
-  homepage 'http://www.darktable.org/'
-  url 'http://downloads.sourceforge.net/project/darktable/darktable/1.0/darktable-1.0.5.tar.gz'
-  version '1.0.5'
-  sha1 'd007ada0f3fb3af6861de74323577b90eec3996e'
-  # --HEAD doesn't actually work atm and I don't feel like fixing it
-  # head 'git://github.com/darktable-org/darktable.git'
+  homepage "http://www.darktable.org/"
+  url "https://github.com/darktable-org/darktable/releases/download/release-1.6.6/darktable-1.6.6.tar.xz"
+  sha256 "f85e4b8219677eba34f5a41e1a0784cc6ec06576326a99f04e460a4f41fd21a5"
 
-  depends_on 'cmake' => :build
-  depends_on 'atk'
-  depends_on 'babl'
-  depends_on 'gegl'
-  depends_on 'exiv2'
-  depends_on 'intltool'
-  depends_on 'gettext'
-  depends_on 'gtk+'
-  depends_on 'jpeg'
-  depends_on 'libtiff'
-  depends_on 'sqlite'
-  depends_on 'openexr'
-  depends_on 'libgphoto2'
-  depends_on 'dbus'
-  depends_on 'fop'
-  depends_on 'librsvg'
-  depends_on 'flickcurl'
-  depends_on 'lensfun'
+  head "https://github.com/darktable-org/darktable.git",
+       :branch => "darktable-1.6.x"
+
+  depends_on "cmake" => :build
+  depends_on "intltool" => :build
+  depends_on "flickcurl" => :optional
+  depends_on "graphicsmagick" => :optional
+  depends_on "libsecret" => :optional
+  depends_on "libsoup" => :optional
+  depends_on "lua" => :optional
+  depends_on "webp" => :optional
+  depends_on "atk"
+  depends_on "babl"
+  depends_on "d-bus"
+  depends_on "exiv2"
+  depends_on "fop"
+  depends_on "gettext"
+  depends_on "gegl"
+  depends_on "gtk+3"
+  depends_on "jpeg"
+  depends_on "json-glib"
+  depends_on "lensfun"
+  depends_on "libgphoto2"
+  depends_on "librsvg"
+  depends_on "libtiff"
+  depends_on "little-cms2"
+  depends_on "openexr"
+  depends_on "sqlite"
   depends_on :x11
 
   def install
@@ -38,11 +43,24 @@ class Darktable < Formula
         "${CMAKE_C_FLAGS}-D_DARWIN_C_SOURCE"
     end
 
-    Dir.mkdir "build"
-    cd "build" do
-      # yep, cmake returns non-0 for success here
-      system "cmake", "..", *std_cmake_args rescue true
-      system "make install"
+    args = %W[
+      --prefix #{prefix}
+      --buildtype Release
+    ]
+    args << "--disable-webp" if build.without? "webp"
+    args << "--disable-lua" if build.without? "lua"
+    args << "--disable-flickr" if build.without? "flickr"
+
+    system "./build.sh", *args
+    cd "./build" do
+      system "make", "install"
     end
+  end
+
+  test do
+    system bin/"darktable-cli", test_fixtures("test.png"),
+           testpath/"out.jpg", "--width", "4px"
+    assert_match "4",
+                 shell_output("sips -g pixelWidth #{testpath}/out.jpg")
   end
 end
