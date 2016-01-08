6548336299dc8b3e9b139b7e41bdf3e4ca244c95
Misty De Meo <mistydemeo@gmail.com>
2016-01-07 23:40:41 -0400

    php1: fix version number

--- php1.rb
+++ php1.rb
@@ -2,7 +2,7 @@ class Php1 < Formula
   desc "Artisinal, ancient PHP"
   homepage "http://php.net"
   url "http://museum.php.net/php1/php-108.tar.gz"
-  version "108"
+  version "1.08"
   sha256 "e7404c9c60021eeec20c77ee75d46db01328f00b549dbf5f6c6f490cca5a211a"
 
   def install
