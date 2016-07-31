838aa9a02d88173980ec601485bf573ab2588cde
Misty De Meo <mistydemeo@gmail.com>
2016-07-31 10:31:01 +1000

    darktable: modernize

--- darktable.rb
+++ darktable.rb
@@ -1,31 +1,26 @@
-require 'formula'
-
 class Darktable < Formula
-  homepage 'http://www.darktable.org/'
-  url 'http://downloads.sourceforge.net/project/darktable/darktable/1.0/darktable-1.0.5.tar.gz'
-  version '1.0.5'
+  homepage "http://www.darktable.org/"
+  url "https://downloads.sourceforge.net/project/darktable/darktable/1.0/darktable-1.0.5.tar.gz"
   sha256 "0c18530446d2f2459fe533a1ef6fc2711300efe7466f36c23168ec2230fb5fbd"
-  # --HEAD doesn't actually work atm and I don't feel like fixing it
-  # head 'git://github.com/darktable-org/darktable.git'
 
-  depends_on 'cmake' => :build
-  depends_on 'atk'
-  depends_on 'babl'
-  depends_on 'gegl'
-  depends_on 'exiv2'
-  depends_on 'intltool'
-  depends_on 'gettext'
-  depends_on 'gtk+'
-  depends_on 'jpeg'
-  depends_on 'libtiff'
-  depends_on 'sqlite'
-  depends_on 'openexr'
-  depends_on 'libgphoto2'
-  depends_on 'dbus'
-  depends_on 'fop'
-  depends_on 'librsvg'
-  depends_on 'flickcurl'
-  depends_on 'lensfun'
+  depends_on "cmake" => :build
+  depends_on "intltool" => :build
+  depends_on "atk"
+  depends_on "babl"
+  depends_on "gegl"
+  depends_on "exiv2"
+  depends_on "gettext"
+  depends_on "gtk+"
+  depends_on "jpeg"
+  depends_on "libtiff"
+  depends_on "sqlite"
+  depends_on "openexr"
+  depends_on "libgphoto2"
+  depends_on "dbus"
+  depends_on "fop"
+  depends_on "librsvg"
+  depends_on "flickcurl"
+  depends_on "lensfun"
   depends_on :x11
 
   def install
@@ -42,7 +37,7 @@ class Darktable < Formula
     cd "build" do
       # yep, cmake returns non-0 for success here
       system "cmake", "..", *std_cmake_args rescue true
-      system "make install"
+      system "make", "install"
     end
   end
 end
