57da964b40f068f95bb5e4303aedf95c98d388a8
Misty De Meo <mistydemeo@gmail.com>
2016-07-31 10:36:30 +1000

    sfnt2woff: modernize

--- sfnt2woff.rb
+++ sfnt2woff.rb
@@ -1,6 +1,5 @@
-require "formula"
-
 class Sfnt2woff < Formula
+  desc "Tool to convert TrueType/OpenType fonts to WOFF"
   homepage "http://people.mozilla.org/~jkew/woff/"
   url "http://people.mozilla.org/~jkew/woff/woff-code-latest.zip"
   sha256 "7713630d2f43320a1d92e2dbb014ca3201caab1dd4c0ab92816016824c680d96"
