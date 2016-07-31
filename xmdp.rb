f43c2a78c8e2a9d892eb93125edda925ef2a8b73
Misty De Meo <mistydemeo@gmail.com>
2016-07-31 10:37:23 +1000

    xmpd: modernize

--- xmdp.rb
+++ xmdp.rb
@@ -1,16 +1,15 @@
-require 'formula'
-
 class Xmdp < Formula
-  homepage 'https://github.com/cmatsuoka/xmdp'
-  url 'https://github.com/cmatsuoka/xmdp.git', :revision => 'e45538c8be5b11302155fd8009fe449aed0acbb7'
-  version 'e45538c8be5b11302155fd8009fe449aed0acbb7'
+  desc "Clone of the FutureCrew MusicDiskPlayer"
+  homepage "https://github.com/cmatsuoka/xmdp"
+  url "https://github.com/cmatsuoka/xmdp.git", :revision => "e45538c8be5b11302155fd8009fe449aed0acbb7"
+  version "e45538c8be5b11302155fd8009fe449aed0acbb7"
 
-  depends_on 'libxmp'
-  depends_on 'sdl'
+  depends_on "libxmp"
+  depends_on "sdl"
 
   def install
     system "make", "CC=#{ENV.cc}", "LD=#{ENV.cc}",
       "CFLAGS=#{ENV.cflags}"
-    bin.install 'xmdp'
+    bin.install "xmdp"
   end
 end
