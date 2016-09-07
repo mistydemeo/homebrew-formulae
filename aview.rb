6dcf15efb953655ddc4176244d1470a351127f86
Misty De Meo <mistydemeo@github.com>
2016-09-08 09:52:39 +1000

    aview: sha256

--- aview.rb
+++ aview.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Aview < Formula
   homepage 'http://aa-project.sourceforge.net/aview/'
   url 'http://downloads.sourceforge.net/aa-project/aview-1.3.0rc1.tar.gz'
-  sha1 'd757ff9f62e6ce4d926c03ae4f4096d9fcf353fc'
+  sha256 "42d61c4194e8b9b69a881fdde698c83cb27d7eda59e08b300e73aaa34474ec99"
 
   depends_on 'aalib'
 
