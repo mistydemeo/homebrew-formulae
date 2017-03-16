9ae9f5f2f039f9a72ecaa234ff75432a632f17e6
Misty De Meo <mistydemeo@gmail.com>
2017-03-16 20:26:08 +1100

    libusb-legacy 0.1.12

---
+++ libusb-legacy.rb
@@ -0,0 +1,23 @@
+class LibusbLegacy < Formula
+  desc "0.1 branch of the library for USB access"
+  homepage "http://libusb.info"
+  url "https://downloads.sourceforge.net/project/libusb/libusb-0.1%20%28LEGACY%29/0.1.12/libusb-0.1.12.tar.gz"
+  sha256 "37f6f7d9de74196eb5fc0bbe0aea9b5c939de7f500acba3af6fd643f3b538b44"
+
+  keg_only "This is an older version of libusb."
+
+  option :universal
+
+  def install
+    ENV.libstdcxx # not even going to *try* libc++ on something this old
+    ENV.universal_binary if build.universal?
+    ENV.deparallelize # 2002, natch
+
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/libusb-config", "--libs"
+  end
+end
