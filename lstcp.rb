5abfb900c15f2a4f2c36d5d59ab8e4a0490898ee
Misty De Meo <mistydemeo@gmail.com>
2017-09-11 19:25:57 -0700

    lstcp 2017-09-11+git6b06b92bc554da3fb8031f3c74ddf1f8aa63406d

---
+++ lstcp.rb
@@ -0,0 +1,10 @@
+class Lstcp < Formula
+  homepage "https://github.com/ticky/dotfiles"
+  url "https://github.com/ticky/dotfiles.git", :revision => "6b06b92bc554da3fb8031f3c74ddf1f8aa63406d"
+  version "2017-09-11+git6b06b92bc554da3fb8031f3c74ddf1f8aa63406d"
+
+  def install
+    bin.install "platform/darwin/bin/lstcp"
+    man1.install "platform/darwin/man/man1/lstcp.1"
+  end
+end
