8d7b4a01c3486fd5b42c1fb986c2915b0e22a9e3
Misty De Meo <mistydemeo@gmail.com>
2018-01-02 19:34:08 +1100

    munt 2.3.0

--- munt.rb
+++ munt.rb
@@ -1,10 +1,11 @@
 class Munt < Formula
   desc "Roland MT-32 emulator library and MIDI conversion tool"
   homepage "https://github.com/munt/munt"
-  url "https://github.com/munt/munt/archive/munt_1_5_0.tar.gz"
-  sha256 "5298b0987df9a27f70232e8a1229949edf84cc7a6dc881817907dee524f7d341"
+  url "https://github.com/munt/munt/archive/munt_2_3_0.tar.gz"
+  sha256 "73bd1f4f67bfec241748ed86d882df12a12b33ac33b9770a65fd37c399c81c2f"
 
   depends_on "cmake" => :build
+  depends_on "glib"
 
   def install
     cd "mt32emu" do
