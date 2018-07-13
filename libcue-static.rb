2a360b60998c5a2315b6d6df0ce272267cfc33dc
Misty De Meo <mistydemeo@gmail.com>
2018-07-12 21:45:11 -0700

    libcue-static: use official static option

--- libcue-static.rb
+++ libcue-static.rb
@@ -1,18 +1,15 @@
 class LibcueStatic < Formula
   desc "Statically-build version of libcue"
   homepage "https://github.com/lipnitsk/libcue"
-  url "https://github.com/lipnitsk/libcue/archive/v2.2.0.tar.gz"
-  sha256 "328f14b8ae0a6b8d4c96928b53b88a86d72a354b4da9d846343c78ba36022879"
+  url "https://github.com/lipnitsk/libcue/archive/v2.2.1.tar.gz"
+  sha256 "f27bc3ebb2e892cd9d32a7bee6d84576a60f955f29f748b9b487b173712f1200"
 
   depends_on "cmake" => :build
 
   keg_only "Copy of libcue library"
 
   def install
-    # Shared library is hardcoded
-    inreplace "CMakeLists.txt", "ADD_LIBRARY(cue SHARED", "ADD_LIBRARY(cue STATIC"
-
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", "-DBUILD_SHARED_LIBS=OFF", *std_cmake_args
     system "make"
     (pkgshare/"tests").mkpath
     cp_r "t/.", pkgshare/"tests"
@@ -23,7 +20,7 @@ class LibcueStatic < Formula
   test do
     cp_r (pkgshare/"tests").children, testpath
     Dir["*.c"].each do |f|
-      system ENV.cc, f, "-o", "test", "-L#{lib}", "-lcue", "-I#{include}"
+      system ENV.cc, f, "-o", "test", "-L#{lib}", "#{lib}/libcue.a", "-I#{include}"
       system "./test"
       rm "test"
     end
