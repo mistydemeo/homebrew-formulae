649fc2a1f9c0818a0892ab007674291a13a2c610
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 20:49:32 -0700

    archivesspace: add license

--- archivesspace.rb
+++ archivesspace.rb
@@ -3,6 +3,7 @@ class Archivesspace < Formula
   url "https://github.com/archivesspace/archivesspace/releases/download/v1.2.0/archivesspace-v1.2.0.zip"
   version "1.2.0"
   sha256 "c555b485c61a593002b9030c52b24245c64c803c392b33fcab9d145b7d8354aa"
+  license "ECL-2.0"
 
   depends_on :java => "1.6+"
 
