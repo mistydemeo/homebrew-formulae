958a9cf2ae64e0d716c7a79b265be761e62bc24a
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 21:01:23 -0700

    cdirip: add license

--- cdirip.rb
+++ cdirip.rb
@@ -3,6 +3,7 @@ class Cdirip < Formula
   homepage "http://web.archive.org/web/20091027000203/http://es.geocities.com/dextstuff/cdirip/news.html"
   url "http://dcemulation.org/1-files/tools/pc/mac/cdirip-0.6.3.zip"
   sha256 "816b1abf724dcc83c7b65d7259735972102a5a60a2a006d5938ef66bebb26acc"
+  license "GPL-2.0"
 
   def install
     system "make"
