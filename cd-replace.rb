02ee7d646d871c1887fb5b914e88a1a2456f815d
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 20:57:33 -0700

    cd-replace: add license

--- cd-replace.rb
+++ cd-replace.rb
@@ -3,6 +3,7 @@ class CdReplace < Formula
   homepage "https://github.com/mistydemeo/cd-replace"
   url "https://github.com/mistydemeo/cd-replace/archive/v0.1.0.tar.gz"
   sha256 "4839230dc4203c01b95847e7a75332def270e547c919c63a6b4ca17bc34dc391"
+  license "GPL-3.0-or-later"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
