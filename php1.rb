05072f4ebb2bbafbaa224f8ed1f98d289d083fcb
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 21:40:07 -0700

    php1: add license

--- php1.rb
+++ php1.rb
@@ -4,6 +4,7 @@ class Php1 < Formula
   url "http://museum.php.net/php1/php-108.tar.gz"
   version "1.08"
   sha256 "e7404c9c60021eeec20c77ee75d46db01328f00b549dbf5f6c6f490cca5a211a"
+  license "GPL-2.0-or-later"
 
   def install
     system "make"
