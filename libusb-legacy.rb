88bece2c52dfcfdcc913522ae1cd599d14fed21f
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 21:28:01 -0700

    libusb-legacy: add license

--- libusb-legacy.rb
+++ libusb-legacy.rb
@@ -3,6 +3,7 @@ class LibusbLegacy < Formula
   homepage "http://libusb.info"
   url "https://downloads.sourceforge.net/project/libusb/libusb-0.1%20%28LEGACY%29/0.1.12/libusb-0.1.12.tar.gz"
   sha256 "37f6f7d9de74196eb5fc0bbe0aea9b5c939de7f500acba3af6fd643f3b538b44"
+  license "LGPL-2.1-or-later"
 
   keg_only "This is an older version of libusb."
 
