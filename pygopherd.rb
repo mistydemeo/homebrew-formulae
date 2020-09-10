0fe2ed68f0bbefb28e87485feff778423820d208
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 21:41:30 -0700

    pygopherd: add license

--- pygopherd.rb
+++ pygopherd.rb
@@ -3,6 +3,7 @@ class Pygopherd < Formula
   homepage "http://gopher.quux.org:70/devel/gopher/pygopherd"
   url "https://github.com/jgoerzen/pygopherd/archive/debian/2.0.18.3.tar.gz"
   sha256 "e13307bd66c7cd0c4d09127e57744f2b32c819c5d5921d1fa6897164f4363e1e"
+  license "GPL-2.0"
 
   def install
     inreplace %w[bin/pygopherd setup.py],
