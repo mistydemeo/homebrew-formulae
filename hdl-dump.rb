1662c7947b7801bad45569f5969f7df013e6bce4
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 21:23:55 -0700

    hdl-dump: moved to github, has a release

--- hdl-dump.rb
+++ hdl-dump.rb
@@ -1,7 +1,9 @@
 class HdlDump < Formula
   desc "CLI tool to interact with Open PS2 Loader over a network"
-  homepage "https://bitbucket.org/AKuHAK/hdl-dump"
-  head "https://bitbucket.org/AKuHAK/hdl-dump", :using => :hg
+  homepage "https://github.com/AKuHAK/hdl-dump"
+  url "https://github.com/AKuHAK/hdl-dump/archive/v47.tar.gz"
+  sha256 "8f946862e645896d298089fb1518ccbb30264fb8bdcd3f4bba0477c0fb893e68"
+  head "https://github.com/AKuHAK/hdl-dump"
 
   def install
     system "make", "RELEASE=yes", "IIN_OPTICAL_MMAP=no"
