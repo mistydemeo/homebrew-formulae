a655c6125393dbbc6d63c63ff74b0b01f6f5efbb
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 20:34:10 -0700

    adx2wav: switch to archived URL

--- adx2wav.rb
+++ adx2wav.rb
@@ -1,6 +1,6 @@
 class Adx2wav < Formula
   homepage "http://www.geocities.co.jp/Playtown/2004/dcdev/index.html"
-  url "http://www.geocities.co.jp/Playtown/2004/dcdev/adx2wav02.lzh"
+  url "https://web.archive.org/web/20190330195533/http://www.geocities.co.jp/Playtown/2004/dcdev/adx2wav02.lzh"
   version "02"
   sha256 "957b5f2029aeda2bcc367478cd484bb375b9cb7ee1749d9025ded8767a5dabfe"
 
