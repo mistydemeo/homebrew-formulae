31805a61d8207bc0b422dcac6f9ab766d1461e00
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 21:53:54 -0700

    wav2adx: switch to archived URLs

--- wav2adx.rb
+++ wav2adx.rb
@@ -1,6 +1,6 @@
 class Wav2adx < Formula
-  homepage "http://www.geocities.co.jp/Playtown/2004/dcdev/index.html"
-  url "http://www.geocities.co.jp/Playtown/2004/dcdev/wav2adx.lzh"
+  homepage "https://web.archive.org/web/20190330195529/http://www.geocities.co.jp/Playtown/2004/dcdev/index.html"
+  url "https://web.archive.org/web/20190330195533/http://www.geocities.co.jp/Playtown/2004/dcdev/wav2adx.lzh"
   sha256 "311e868ea03aa4abd1ea2d46e08f34d5b29f436a5f51e54bb47a07fd4c77ef4f"
   version "2001-01-25"
 
