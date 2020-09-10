f58799e65ffd28af0c327208909084bc99e06da0
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 20:39:08 -0700

    applyppf3: a few style fixes

--- applyppf3.rb
+++ applyppf3.rb
@@ -1,9 +1,8 @@
 class Applyppf3 < Formula
-  desc ""
+  desc "Tools to use and create PPF3 patches"
   homepage ""
-  url "http://rescene.wdfiles.com/local--files/ppf-o-matic/pdx-ppf3.zip"
+  url "https://rescene.wdfiles.com/local--files/ppf-o-matic/pdx-ppf3.zip"
   sha256 "5201d979e720d59750a1341fac9cb2a1f1d57dc4cf49218f6c17a1b560ee1213"
-  # depends_on "cmake" => :build
 
   def install
     cd "ppfdev" do
@@ -20,15 +19,6 @@ class Applyppf3 < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test applyppf3`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    assert_match "ApplyPPF v3.0 by =Icarus/Paradox=", shell_output("#{bin}/applyppf3")
   end
 end
