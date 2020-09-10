5f80e1c7ca8a3c05e6d8430e416963cf335b122d
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 21:26:23 -0700

    jit: add license

--- jit.rb
+++ jit.rb
@@ -3,6 +3,7 @@ class Jit < Formula
   homepage "https://shop.jcoglan.com/building-git/"
   url "https://rubygems.org/downloads/jit-1.0.0.gem"
   sha256 "c0a45ddd7f1ce41ab97d654b7e11911198790f8cb0e09896d8968159f673b1a1"
+  license "GPL-3.0"
 
   def install
     # This is a gem, but it has no dependencies, so we can
