0d339ab58920a2c66b28e5854e6244291cd739fb
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 20:59:28 -0700

    cdirip: switch to archived homepage

--- cdirip.rb
+++ cdirip.rb
@@ -1,6 +1,6 @@
 class Cdirip < Formula
   desc "Tool to extract tracks from DiscJuggler images"
-  homepage "http://es.geocities.com/dextstuff/cdirip/news.html"
+  homepage "http://web.archive.org/web/20091027000203/http://es.geocities.com/dextstuff/cdirip/news.html"
   url "http://dcemulation.org/1-files/tools/pc/mac/cdirip-0.6.3.zip"
   sha256 "816b1abf724dcc83c7b65d7259735972102a5a60a2a006d5938ef66bebb26acc"
 
