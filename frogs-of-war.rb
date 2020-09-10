55a7a2dc7d1073bd94a330993a8006571726f17d
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 21:18:21 -0700

    frogs-of-war: add license

--- frogs-of-war.rb
+++ frogs-of-war.rb
@@ -3,6 +3,7 @@ class FrogsOfWar < Formula
   homepage "http://web.archive.org/web/20190114033419/http://www.fullsack.com/frogs/frogs.html"
   url "http://web.archive.org/web/20190428032533/http://www.fullsack.com/frogs/fow-2.2.tar.gz"
   sha256 "5ba216d5df2b8b3bfb9709d6a487e986f18a931c8e2ae519828363742b576999"
+  license "GPL-2.0-or-later"
 
   depends_on "sdl"
   depends_on "sdl_image"
