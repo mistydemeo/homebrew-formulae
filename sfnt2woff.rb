611bfa97610ff5c78bee805a4cd001a3e9267a59
Misty De Meo <mistydemeo@gmail.com>
2020-09-09 21:46:44 -0700

    sfnt2woff: switch to archived URLs

--- sfnt2woff.rb
+++ sfnt2woff.rb
@@ -1,7 +1,7 @@
 class Sfnt2woff < Formula
   desc "Tool to convert TrueType/OpenType fonts to WOFF"
-  homepage "http://people.mozilla.org/~jkew/woff/"
-  url "http://people.mozilla.org/~jkew/woff/woff-code-latest.zip"
+  homepage "http://web.archive.org/web/20161201080702/https://people-mozilla.org/~jkew/woff/"
+  url "https://web.archive.org/web/20160304124743/http://people.mozilla.org/~jkew/woff/woff-code-latest.zip"
   sha256 "7713630d2f43320a1d92e2dbb014ca3201caab1dd4c0ab92816016824c680d96"
   version "2009-10-04"
 
