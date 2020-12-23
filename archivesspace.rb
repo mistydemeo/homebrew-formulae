cb08475642c2e20c1c43f1c7649365b62c50cc3b
Misty De Meo <mistydemeo@gmail.com>
2020-12-22 21:20:06 -0800

    archivesspace: use openjdk

--- archivesspace.rb
+++ archivesspace.rb
@@ -5,7 +5,7 @@ class Archivesspace < Formula
   sha256 "c555b485c61a593002b9030c52b24245c64c803c392b33fcab9d145b7d8354aa"
   license "ECL-2.0"
 
-  depends_on :java => "1.6+"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
